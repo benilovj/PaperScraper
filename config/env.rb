00f1378d0263208db41c0a706d9099c75f0afad4
benilovj <benilov@gmail.com>
2011-08-26 23:33:52 +0200

    fix for 1.8.7 inconsistency

--- config/env.rb
+++ config/env.rb
@@ -4,11 +4,14 @@ require 'logger'
 require 'active_record'
 require 'fileutils'
 
-$: << File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib')
+BASE_PATH = File.join(File.expand_path(File.dirname(__FILE__)), '..')
+
+$: << File.join(BASE_PATH, 'lib')
 
 RAILS_ENV = ENV['RAILS_ENV'] || 'development'
 dbconf = YAML::load(File.open('config/databases.yml'))[RAILS_ENV]
 ActiveRecord::Base.establish_connection(dbconf)
 
-FileUtils.mkdir('log') unless Dir.exists?('log')
-ActiveRecord::Base.logger = Logger.new(File.open('log/database.log', 'a'))
+LOG_PATH = File.join(BASE_PATH, 'log')
+FileUtils.mkdir(LOG_PATH) unless File.directory?(LOG_PATH)
+ActiveRecord::Base.logger = Logger.new(File.open(File.join(LOG_PATH, 'database.log'), 'a'))
