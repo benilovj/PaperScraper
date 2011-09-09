3e2e7c498c53aac3b22e55e3254280f1c1608c2c
benilovj <benilov@gmail.com>
2011-09-10 00:07:41 +0200

    fixed broken ajax post

--- public/comment-ajax.js
+++ public/comment-ajax.js
@@ -1,31 +1,23 @@
 $(document).ready(function() {
     $('#response').hide();
 
-    $("#left-form").submit(function(event) {
-		event.preventDefault(); 
+    var ajaxPost = function(event) {
+        event.preventDefault();
         $.ajax({
             type: "POST",
-            url: "/game/answer/Daily%20Mail",
+            url: event.target.getAttribute( 'action' ),
             success: function(html) { 
-                $("#response").append(html); }
+                $("#response").append(html);
+            }
         });
         $('#response').show();
         hideButtons();
-	})
-	
-    $("#right-form").submit(function(event) {
-		event.preventDefault(); 
-        $.ajax({
-            type: "POST",
-            url: "/game/answer/Guardian",
-            success: function(html) { 
-                $("#response").append(html); }
-        });
-        $('#response').show();
-        hideButtons();
-	})
+    }
 
-	function hideButtons() {
-	    $("#wflf-selection-buttons").hide();
-	}
+    $("#left-form").submit(ajaxPost);
+    $("#right-form").submit(ajaxPost);
+
+    function hideButtons() {
+        $("#wflf-selection-buttons").hide();
+    }
 });
